0431cc26bbf73ca3652eec73dd2c24e2498551ee
yaman <yemenzohir@gmail.com>
2026-01-05 22:34:46 +0300

    Turing The Context into Provvider

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import TodoList from "./Todolist";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { TodosContext } from "./Contexts/todoContext";
 import { useState } from "react";
-import MySnackbar from "./MySnackbar";
-import { MySnackbarContext } from "./Contexts/MySnackbarContext";
+import { ToastProvider } from "./Contexts/MySnackbarContext";
 
 // Create a UUID
 import { v4 as uuidv4 } from "uuid";
@@ -41,30 +40,16 @@ function App() {
       isCompleted: false,
     },
   ]);
-  const [open, setOpen] = useState(false);
-  const [message, setmessage] = useState("");
 
-  const handleClick = (message) => {
-    setOpen(true);
-    setmessage(message)
-  };
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-
-    setOpen(false);
-  };
   return (
     <ThemeProvider theme={theme}>
-      <MySnackbarContext.Provider value={{ handleClick }}>
+      <ToastProvider>
         <div className="App" style={{ direction: "rtl" }}>
           <TodosContext.Provider value={{ Todos, setTodos }}>
             <TodoList />
-            <MySnackbar open={open} handleClose={handleClose}  message={message}/>
           </TodosContext.Provider>
         </div>
-      </MySnackbarContext.Provider>
+      </ToastProvider>
     </ThemeProvider>
   );
 }
--- src/Contexts/MySnackbarContext.js
+++ src/Contexts/MySnackbarContext.js
@@ -1,2 +1,26 @@
-import { createContext } from "react";
-export const MySnackbarContext = createContext({})
+import { createContext, useState } from "react";
+import MySnackbar from "../MySnackbar";
+export const MySnackbarContext = createContext({});
+export const ToastProvider = ({ children }) => {
+  const [open, setOpen] = useState(false);
+  const [message, setmessage] = useState("");
+
+  const handleClick = (message) => {
+    setOpen(true);
+    setmessage(message);
+  };
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+  return (
+    <MySnackbarContext.Provider value={{ handleClick }}>
+      <MySnackbar open={open} handleClose={handleClose} message={message} />
+
+      {children}
+    </MySnackbarContext.Provider>
+  );
+};
